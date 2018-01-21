e2f38d7f54e15634c8028d00e3cd3c5de5e8dd4d
Gang Zhang <gzhangx@hotmail.com>
2018-01-21 13:37:11 -0500

    2ports

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import RPi.GPIO as GPIO
 import time
 GPIO.VERSION
 GPIO.setmode(GPIO.BOARD)
-#GPIO.setwarnings(False)
+GPIO.setwarnings(False)
 ledPin = 29
+pin2 = 31
 GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(pin2, GPIO.OUT)
 GPIO.output(ledPin, GPIO.HIGH)
 GPIO.output(ledPin, GPIO.LOW)
 
@@ -13,8 +15,10 @@ ledSleep = 0.5
 try:
   while 1:
     GPIO.output(ledPin, GPIO.HIGH)
+    GPIO.output(pin2, GPIO.HIGH)
     time.sleep(ledSleep)
     GPIO.output(ledPin, GPIO.LOW)
+    GPIO.output(pin2, GPIO.LOW)
     time.sleep(ledSleep)
 except KeyboardInterrupt:
   GPIO.cleanup()
